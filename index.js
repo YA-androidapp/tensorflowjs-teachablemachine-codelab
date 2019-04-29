fdcdb8e9c87ee679e70d731e0ca8f2f08a1e523f
YA <YA-androidapp@users.noreply.github.com>
2019-04-29 11:05:25 +0900

    Add counter

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 ﻿let net;
 const classifier = knnClassifier.create();
 const webcamElement = document.getElementById('webcam');
+const buttons = new Array('class-a', 'class-b', 'class-c')
 
 async function app() {
   console.log('Loading mobilenet..');
@@ -13,19 +14,30 @@ async function app() {
 
   // Reads an image from the webcam and associates it with a specific class
   // index.
+  const splitStr = ' / '
   const addExample = classId => {
+    console.log('classId: ' + classId)
+
     // Get the intermediate activation of MobileNet 'conv_preds' and pass that
     // to the KNN classifier.
     const activation = net.infer(webcamElement, 'conv_preds');
 
     // Pass the intermediate activation to the classifier.
     classifier.addExample(activation, classId);
+
+    let buttonElement = document.getElementById(buttons[classId]);
+    let spt = buttonElement.innerText.split(splitStr)
+    buttonElement.innerText =
+      spt[0] + splitStr +
+      (
+        (spt[1]) == undefined ? 1 : parseInt((spt[1]).trim()) + 1
+      );
   };
 
   // When clicking a button, add an example for that class.
-  document.getElementById('class-a').addEventListener('click', () => addExample(0));
-  document.getElementById('class-b').addEventListener('click', () => addExample(1));
-  document.getElementById('class-c').addEventListener('click', () => addExample(2));
+  document.getElementById(buttons[0]).addEventListener('click', () => addExample(0));
+  document.getElementById(buttons[1]).addEventListener('click', () => addExample(1));
+  document.getElementById(buttons[2]).addEventListener('click', () => addExample(2));
 
   while (true) {
     if (classifier.getNumClasses() > 0) {
@@ -36,7 +48,7 @@ async function app() {
 
       const classes = ['A', 'B', 'C'];
       document.getElementById('console').innerText = `
-        prediction: ${classes[result.classIndex]}\n
+        prediction: ${classes[result.classIndex]}\t
         probability: ${result.confidences[result.classIndex]}
       `;
     }
@@ -49,13 +61,15 @@ async function setupWebcam() {
   return new Promise((resolve, reject) => {
     const navigatorAny = navigator;
     navigator.getUserMedia = navigator.getUserMedia ||
-        navigatorAny.webkitGetUserMedia || navigatorAny.mozGetUserMedia ||
-        navigatorAny.msGetUserMedia;
+      navigatorAny.webkitGetUserMedia || navigatorAny.mozGetUserMedia ||
+      navigatorAny.msGetUserMedia;
     if (navigator.getUserMedia) {
-      navigator.getUserMedia({video: true},
+      navigator.getUserMedia({
+          video: true
+        },
         stream => {
           webcamElement.srcObject = stream;
-          webcamElement.addEventListener('loadeddata',  () => resolve(), false);
+          webcamElement.addEventListener('loadeddata', () => resolve(), false);
         },
         error => reject());
     } else {
